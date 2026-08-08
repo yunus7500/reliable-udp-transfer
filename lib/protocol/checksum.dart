9da3e78a5113ef47654be0685236d2c1296fe69a
Yunus Can <sevinc60178@gmail.com>
2026-08-08 16:33:11 +0300

    feat: integrate crc32 checksum calculation to packets

--- lib/protocol/checksum.dart
+++ lib/protocol/checksum.dart
@@ -1,1 +1,14 @@
 // CRC32 calculation
+
+import 'dart:typed_data';
+import 'package:crclib/catalog.dart';
+
+class Checksum {
+  // verilen byte dizisinin CRC32(hata kontrol) kodunu hesaplar
+
+  static int calculate(Uint8List data) {
+    // crclib paketindeki standart Crc32 algoritması
+    final crc = Crc32().convert(data);
+    return crc.toBigInt().toInt();
+  }
+}
--- lib/protocol/packet.dart
+++ lib/protocol/packet.dart
@@ -1,22 +1,48 @@
 // Packet header/payload encode-decode
 
 import 'dart:typed_data';
+import 'package:reliable_udp_transfer/protocol/checksum.dart' show Checksum;
+
 import 'packet_type.dart';
 import 'constants.dart';
 
 class Packet {
   final int sequenceNumber; // paket sırası
-  final int checksum; // veri bütünlüğü doğrulama kodu
-
   final PacketType type; // paket tipi( data,ack,handshake vs.)
   final Uint8List payload; // taşıdığı veri(dosya parçası)
+  late final int checksum; // veri bütünlüğü doğrulama kodu
 
-  const Packet({
+  Packet({
     required this.sequenceNumber,
     required this.type,
     required this.payload,
-    this.checksum = 0, // ilk olarak =0,bir sonraki aşamada hesaplanacak
-  });
+    int? providedChecksum, // ilk olarak =0,bir sonraki aşamada hesaplanacak
+  }) {
+    // eğer checksum verilmişse (mesela paketi ağdan okuyorsak) onu kullan
+    if (providedChecksum != null) {
+      checksum = providedChecksum;
+    } else {
+      // eğer sıfırdan biz gönderiyosak verileri birleştirip hesapla
+      checksum = _calculateChecksum();
+    }
+  }
+
+  int _calculateChecksum() {
+    // Checksum hesabına ne girecek ? Sequence(5) + Type(1) + Length(4) = 9 byte header ve payload
+    int length = 9 + payload.length;
+    ByteData temp = ByteData(length);
+
+    temp.setUint32(0, sequenceNumber, Endian.big);
+    temp.setUint8(4, type.value);
+    temp.setUint32(5, payload.length, Endian.big);
+
+    Uint8List dataToHash = Uint8List(length);
+    dataToHash.setAll(0, temp.buffer.asUint8List());
+    dataToHash.setAll(9, payload);
+
+    // oluşturduğumuz saf veriyi checksum sınıfına yollayıp sayıyı alıyoruz
+    return Checksum.calculate(dataToHash);
+  }
 
   /// paketi UDP soketinden gönderebilmek için byte dizisine dönüştürür
 
@@ -49,16 +75,16 @@ class Packet {
     }
     ByteData byteData = ByteData.sublistView(bytes);
 
-    // Header verilerini sırayla oku
+    // header verilerini sırayla oku
     int seq = byteData.getUint32(0, Endian.big);
     int typeValue = byteData.getUint8(4);
     int payloadLength = byteData.getUint32(5, Endian.big);
     int checksum = byteData.getUint32(9, Endian.big);
 
-    // Byte'tan okunan tipi PacketType enum'una çevir
+    // byte'tan okunan tipi PacketType enum'una çevir
     PacketType type = PacketType.fromValue(typeValue);
 
-    // Payload'ı asıl dizinin içinden kesip al
+    // payload'ı asıl dizinin içinden kesip al
     Uint8List payload = bytes.sublist(
       ProtocolConstants.headerSize,
       ProtocolConstants.headerSize + payloadLength,
@@ -67,7 +93,8 @@ class Packet {
       sequenceNumber: seq,
       type: type,
       payload: payload,
-      checksum: checksum,
+
+      providedChecksum: checksum,
     );
   }
 }
